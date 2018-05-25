677ecc00055335b78ec92a153f5ed5256fa46bb2
sridharbandi <sridhar.bandi.ece@gmail.com>
2018-05-25 16:41:54 +0100

    Added drivers

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,8 @@
 const seleniumServer = require('selenium-server');
 const chromedriver = require('chromedriver');
 const geckodriver = require('geckodriver');
+const iedriver = require('iedriver');
+const edgedriver = require('edgedriver');
 
 module.exports = {
     src_folders: ["tests"],
@@ -52,6 +54,30 @@ module.exports = {
             selenium: {
                 cli_args: {
                     'webdriver.gecko.driver': geckodriver.path
+                }
+            }
+        },
+        ie: {
+            desiredCapabilities: {
+                browserName: 'internet explorer',
+                javascriptEnabled: true,
+                acceptSslCerts : true
+            },
+            selenium: {
+                cli_args: {
+                    'webdriver.ie.driver': iedriver.path
+                }
+            }
+        },
+        edge: {
+            desiredCapabilities: {
+                browserName: 'MicrosoftEdge',
+                javascriptEnabled: true,
+                acceptSslCerts : true
+            },
+            selenium: {
+                cli_args: {
+                    'webdriver.edge.driver': edgedriver.path
                 }
             }
         }
